27b48ff7269f2e4cc494c70f7a75419831fa20ff
alexwei77 <alexwei677@gmail.com>
2023-06-07 02:33:37 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Edited files
+
+components/Notes
+App.js
+index.js
+serviceWorker.js(created)
+public/manifest.json
+
---
+++ src/App.js
@@ -0,0 +1,32 @@
+// import logo from './logo.svg';
+import React, { useState } from 'react';
+import Notes from './components/Notes';
+import './App.css';
+
+function App() {
+
+  let [list, setList] = useState([]);
+  let [newNote, setNewNote] = useState('');
+
+  function addNote() {
+    let addedNote = list.concat(newNote);
+    setList(addedNote);
+    setNewNote('');
+  }
+
+  function handleChange(e) {
+    setNewNote(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h1>My Notes</h1>
+      <input type="text" value={newNote} onChange={handleChange} />
+
+      <button onClick={addNote}>Save</button>
+      <Notes data={list} />
+    </div>
+  );
+}
+
+export default App;
